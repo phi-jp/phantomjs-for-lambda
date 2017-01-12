63a5c75249b6e48d9c3f0eaa6d96cc1d532a3aab
phi <phi1618jp@gmail.com>
2017-01-13 01:48:21 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+var path = require('path');
+
+exports.path = path.resolve(__dirname, 'bin/phantomjs');
+
+exports.version = '2.1.1';
---
+++ test/test.js
@@ -0,0 +1,3 @@
+var phantomjs = require('../index.js');
+
+console.log(phantomjs);
